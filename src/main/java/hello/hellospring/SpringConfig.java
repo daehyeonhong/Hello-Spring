7fbddf0b3a4f1f482253acb4907b0de61bf0499c
DaeHyeonHong <fholng@outlook.com>
2021-02-12 02:12:12 +0900

    Spring Data JPA

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -15,30 +15,43 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-    /*private DataSource dataSource;
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /*
+    private DataSource dataSource;
 
     @Autowired
     public SpringConfig(DataSource dataSource) {
         this.dataSource = dataSource;
-    }*/
-    private EntityManager entityManager;
+    }
+    */
+   /*
+   private EntityManager entityManager;
 
     @Autowired
     public SpringConfig(EntityManager entityManager) {
         this.entityManager = entityManager;
     }
+    */
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
+    /*
     @Bean
     public MemberRepository memberRepository() {
-//  return new MemoryMemberRepository();
-//        return new JDBCMemberRepository(dataSource);
-        //      return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(entityManager);
+        //  return new MemoryMemberRepository();
+        //  return new JDBCMemberRepository(dataSource);
+        //  return new JdbcTemplateMemberRepository(dataSource);
+        //  return new JpaMemberRepository(entityManager);
     }
+    */
 
 }
---
+++ src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,14 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+
+    // JPQL SELECT M FROM Member M WHERE M.NAME = ?
+    @Override
+    Optional<Member> findByName(String name);
+
+}
