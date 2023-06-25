5950c59582f1d8ca56e5db4f860f1c51b72e0dc8
HongDaeHyeon <fholng@outlook.com>
2023-06-25 11:54:27 +0900

    mvc 기본 controller -> view

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -10,5 +11,12 @@ public class HelloController {
     public String hello(final Model model) {
         model.addAttribute("data", "hello!!");
         return "hello";
+    }
+
+    @GetMapping(value = "/hello-mvc")
+    public String helloMvc(@RequestParam(value = "name") final String name,
+                           final Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
