f2c99a71f9958e614f920a81b04f7da742cc25ad
HongDaeHyeon <fholng@outlook.com>
2023-06-26 23:07:11 +0900

    회원가입 기능 생성

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,14 +1,34 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.damain.Member;
 import hello.hellospring.service.MemberService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
+    private static final Logger log = LogManager.getLogger(MemberController.class);
     private final MemberService memberService;
 
     public MemberController(final MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping(value = "/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping(value = "/members/new")
+    public String create(final MemberForm form) {
+        log.info("form={}", form);
+        final var member = new Member();
+        member.setName(form.name());
+        log.info("member={}", member);
+        this.memberService.join(member);
+        return "redirect:/";
     }
 }
---
+++ src/main/java/hello/hellospring/controller/MemberForm.java
@@ -0,0 +1,6 @@
+package hello.hellospring.controller;
+
+public record MemberForm(
+        String name
+) {
+}
